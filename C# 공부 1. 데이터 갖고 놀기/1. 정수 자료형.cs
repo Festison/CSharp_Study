2acf4e8d8e4f3daedf802393def1519ed2ee4294
Festison <105289311+Festison@users.noreply.github.com>
2022-09-05 20:40:27 +0900

    Add files via upload

---
+++ C# 공부 1. 데이터 갖고 놀기/1. 정수 자료형.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C샵_공부_1._데이터_갖고_놀기
+{
+    class Program
+    {
+        static void sub1(string[] args)
+        {
+            // 데이터  + 로직
+            // 체력 0 = 죽음
+
+            // byte(1바이트 0~255), short(2바이트) , int(4바이트) , long(8바이트)
+            // sbyte(1바이트 -128~127), ushort, uint, ulong
+            int hp = 100;
+            short level = 100;
+            long id; // id 는 long으로 만드는게 좋다.
+
+            byte attack = 0;
+
+            // 10 진수
+            // 0 1 2 3 4 5 6 7 8 9 10
+
+            // 2진수
+            // 0 1
+
+            // 16 진수
+            // 0~9 A B C D E F
+            // 0x00 0x01 0x02 .. 0x0F
+            // 0x10 0x11 0x12 .. 0x1F
+
+            Console.WriteLine("Hello Number ! {0}", hp);
+        }
+    }
+}
---
+++ C# 공부 1. 데이터 갖고 놀기/2. 불리언,소수,문자,문자열 형식.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C샵_공부_1._데이터_갖고_놀기
+{
+    class Class1
+    {
+        static void sub2(string[] args)
+        {
+            // 1바이트 (참/ 거짓)
+            bool b;
+            b = true;
+            b = false;
+
+            // 소수
+            float f = 3.14f;        // 4바이트
+            double d = 3.14;      // 8바이트
+
+            // 2바이트
+            char c = 'a';
+            string str = "Hello World!";
+
+            Console.WriteLine(str);
+
+        }
+    }
+}
