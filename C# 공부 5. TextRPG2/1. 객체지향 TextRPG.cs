a22f9869ac74e215a223561e4175d019decfe635
Festison <105289311+Festison@users.noreply.github.com>
2022-09-13 23:03:32 +0900

    Add files via upload

---
+++ C# 공부 5. TextRPG2/1. 객체지향 TextRPG.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_공부_5.TextRPG2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while (true)
+            {
+                game.process();
+            }
+        }
+    }
+}
---
+++ C# 공부 5. TextRPG2/4. Creature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C샵_공부_5.TextRPG2
+{
+    public enum CreatureType
+    {
+        None,
+        Player = 1,
+        Monster = 2
+    }
+
+    class Creature
+    {
+        CreatureType type;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool IsDead() { return hp <= 0; }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+    }
+}
