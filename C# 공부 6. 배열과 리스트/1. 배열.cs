a9931f7868b41f3a0a45cd71a3d87f422248eaa9
Festison <105289311+Festison@users.noreply.github.com>
2023-03-10 11:52:49 +0900

    Add files via upload

---
+++ C# 공부 6. 배열과 리스트/1. 배열.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSahrp_공부
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.WriteLine(i*j);
+                }
+            }
+            
+        }
+    }
+}
