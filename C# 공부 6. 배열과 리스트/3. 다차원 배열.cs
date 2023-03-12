15785f367379b73dc65f3db56fc99bd1fcce9883
Festison <105289311+Festison@users.noreply.github.com>
2023-03-12 17:16:02 +0900

    Add files via upload

---
+++ C# 공부 6. 배열과 리스트/3. 다차원 배열.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CSahrp_공부
+{
+    class Program
+    {
+        class Map
+        {
+            int[,] tiles =
+            {
+                { 1, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1 },
+            };
+
+            public void Render()
+            {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+
+                for (int y = 0; y < tiles.GetLength(1); y++)
+                {
+                    for (int x = 0; x < tiles.GetLength(0); x++)
+                    {
+                        if (tiles[y,x]==1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write('\u25cf');
+                    }
+                    Console.WriteLine();
+                }
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+          static void Main(string[] args)
+        {
+            int[] scores = new int[5] { 10, 30, 40, 20, 50 };
+
+            // 2차원 배열
+            int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            Map map = new Map();
+            map.Render();
+
+  
+        }
+    }
+}
