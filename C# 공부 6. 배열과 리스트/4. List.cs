af426af3067c575069c3a140f8b00ff79a4125c4
Festison <105289311+Festison@users.noreply.github.com>
2023-03-13 15:42:09 +0900

    Add files via upload

---
+++ C# 공부 6. 배열과 리스트/4. List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSahrp_공부
+{
+    class Program
+    {   
+          static void Main(string[] args)
+        {
+            // List는 동적 배열로 이루어져있다.
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i); // 제일 뒤에 대입
+            }
+
+            // 삽입 삭제
+            list.Insert(2, 999);
+            list.Remove(3);
+            list.RemoveAt(0); // 첫번째 요소 삭제
+            list.Clear(); // 전체 삭제
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            foreach (int num in list)
+            {
+                Console.WriteLine(num);
+            }
+          
+        }
+    }
+}
