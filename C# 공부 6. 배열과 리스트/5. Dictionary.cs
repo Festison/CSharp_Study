0884e9475a0aaa5388c26a7458603d1e051dd6be
Festison <105289311+Festison@users.noreply.github.com>
2023-03-13 18:13:44 +0900

    Add files via upload

---
+++ C# 공부 6. 배열과 리스트/5. Dictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSahrp_공부
+{
+    class Monster
+    {
+        public Monster(int id) { this.id = id; }
+
+        public int id;
+    }
+    class Program
+    {   
+        // Dictionary는 Hashtable을 이용
+        static void Main(string[] args)
+        {
+            Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                dic.Add(i, new Monster(i));
+            }
+
+            Monster mon;
+            bool found = dic.TryGetValue(7777, out mon);
+
+            dic.Remove(7777);
+            
+        }
+    }
+}
