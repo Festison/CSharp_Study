6b71fecbe974b9dc53af74266e1200f54d17daf8
Festison <105289311+Festison@users.noreply.github.com>
2023-03-14 14:45:55 +0900

    Add files via upload

--- C# 공부 7. 기타 문법/2. Interface(인터페이스).cs
+++ C# 공부 7. 기타 문법/2. Interface(인터페이스).cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace _7._기타_문법
 {
-    class _2
+    class Interface
     {
         abstract class Monster // 추상 클래스 : 더 이상 생성 불가능
         {
-            public abstract void Shout();
+            public abstract void Shout(); // 함수 또한 추상적으로 만들 수 있으며 어떠한 기능을 반드시 사용해야할 경우 사용
         }
 
-        interface IFlyable
+        interface IFlyable // 인터페이스를 사용해 이중상속을 가능하게 한다.
         {
             void Fly();
         }
 
         class Orc : Monster
         {
-            public override void Shout()
+            public override void Shout() // 추상 클래스의 함수를 반드시 오버라이드 해야한다.
             {
                 Console.WriteLine("");
             }
