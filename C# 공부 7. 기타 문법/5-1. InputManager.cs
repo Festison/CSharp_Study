3c3844a6e42b1fd5a634e5580a7cc3a920065639
Festison <105289311+Festison@users.noreply.github.com>
2023-03-14 17:52:51 +0900

    Add files via upload

---
+++ C# 공부 7. 기타 문법/5-1. InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._기타_문법
+{
+    class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if (Console.KeyAvailable==false)
+            {
+                return;
+            }
+
+            ConsoleKeyInfo info = Console.ReadKey();
+
+            if (info.Key == ConsoleKey.A) 
+            {
+                // 모두한테 알려준다
+                InputKey();
+            }
+        }
+    }
+}
