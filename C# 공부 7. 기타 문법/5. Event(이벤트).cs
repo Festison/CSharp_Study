8d8cc1923b3667238c97f6e0c751e5919b8cbf33
Festison <105289311+Festison@users.noreply.github.com>
2023-03-14 17:52:37 +0900

    Add files via upload

---
+++ C# 공부 7. 기타 문법/5. Event(이벤트).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._기타_문법
+{
+    class Event
+    {
+        static void OnInputTest()
+        {
+            Console.WriteLine("Input Received");
+        }
+
+        static void Main(string[] args)
+        {
+            InputManager inputManager = new InputManager();
+
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
+         
+        }
+    }
+}
