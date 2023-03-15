17ce5a1035bd714d13e21fad28b439d167b6870b
Festison <105289311+Festison@users.noreply.github.com>
2023-03-15 18:25:02 +0900

    Add files via upload

---
+++ C# 공부 7. 기타 문법/9. Nullable(널러블).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._기타_문법
+{
+    
+    class Nullable
+    {
+        static int Find()
+        {
+            return 0;
+        }
+
+        class Monster
+        {
+            public int Id { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            // Nullable -> Null + able
+            Monster monster = null;
+
+            if (monster != null) 
+            {
+                int monsterId = monster.Id;
+            }
+
+            int? id = monster?.Id; // monster값이 null이 아닐시 값을 넣고 null일시 null을 넣는다.
+        }
+    }
+}
