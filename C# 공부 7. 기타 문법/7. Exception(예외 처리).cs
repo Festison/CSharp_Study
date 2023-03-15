32382c405aa96531da2f510b43f79f11264414f9
Festison <105289311+Festison@users.noreply.github.com>
2023-03-15 18:24:28 +0900

    Add files via upload

---
+++ C# 공부 7. 기타 문법/7. Exception(예외 처리).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._기타_문법
+{
+    class exception
+    {
+        class TestException : Exception
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 1. 0으로 나눌 떄
+                // 2. 잘못된 메모리를 참조 (null)
+                // 3. 오버플로우
+                int a = 10;
+                int b = 0;
+                int result = a / b;
+
+                int c = 0;
+
+                throw new TestException();
+            }
+            catch (DivideByZeroException e)
+            {
+
+               
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally // 무조건 한번 실행되는 부분
+            {
+                // DB, 파일 정리 할때 사용
+            }
+        }
+    }
+}
